a1eb9d0425d934f23d20f508c9000a2f6a30cbd0
jeff <jeff563715@gmail.com>
2025-09-12 09:51:22 +0800

    sopport multi reward logging

--- verl/trainer/ppo/ray_trainer.py
+++ verl/trainer/ppo/ray_trainer.py
@@ -1116,6 +1116,11 @@ class RayPPOTrainer:
                             )
                         else:
                             reward_tensor, reward_extra_infos_dict = compute_reward(batch, self.reward_fn)
+                        
+                            for key in reward_extra_infos_dict:
+                                if key != "score":
+                                    this_val = np.array(reward_extra_infos_dict[key])
+                                    metrics.update({f"critic/rewards/{key}": np.mean(this_val)})
 
                     # Operating Mode Selection:
                     # - Bypass mode: Sets old_log_probs = rollout_log_probs (2 policies: π_rollout, π_θ)
